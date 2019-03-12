c7e55a406726a5a851ce1a14944d4c8da8d7eb0e
Saahiththiyan Mathivathanan <saahimathi@gmail.com>
2019-03-12 20:59:10 +0530

    adding user feature created

--- public/create.php
+++ public/create.php
@@ -1,4 +1,36 @@
-<?php include "templates/header.php"?>
+<?php include "templates/header.php";
+    require "../common.php";
+    if (isset($_POST['submit'])) {
+        require "../config.php";
+
+        try {
+            $connection = new PDO($dsn, $username, $password, $options);
+            $new_user = array(
+                "firstname" => $_POST['firstname'],
+                "lastname"  => $_POST['lastname'],
+                "email"     => $_POST['email'],
+                "age"       => $_POST['age'],
+                "location"  => $_POST['location']
+            );
+            $sql = sprintf(
+                "INSERT INTO %s (%s) values (%s)",
+                "users",
+                implode(", ", array_keys($new_user)),
+                ":" . implode(", :", array_keys($new_user))
+            );
+            $statement = $connection->prepare($sql);
+            $statement->execute($new_user);
+    
+        } catch(PDOException $error) {
+            echo $sql . "<br>" . $error->getMessage();
+        } 
+    }
+    
+?>
+
+<?php if (isset($_POST['submit']) && $statement) { ?>
+    <blockquote><?php echo escape($_POST['firstname']); ?> successfully added.</blockquote>
+<?php } ?>
 
 <h2>Add a user</h2>
 
